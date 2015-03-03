0797e3e1555fc422eefe742fbca554cc62606e28
Colin Mathews <colin.mathews@gmail.com>
2015-03-03 10:00:03 -0500

    Logging support for errors and warnings

--- logs/attributes/default.rb
+++ logs/attributes/default.rb
@@ -1,1 +1,3 @@
 default[:cwlogs][:logfile] = '/var/log/app'
+default[:cwlogs][:logfileErrors] = '/var/log/app-errors'
+default[:cwlogs][:logfileWarnings] = '/var/log/app-warnings'
--- logs/recipes/install.rb
+++ logs/recipes/install.rb
@@ -7,7 +7,7 @@ remote_file "/opt/aws/cloudwatch/awslogs-agent-setup.py" do
   mode "0755"
 end
  
-  execute "Install CloudWatch Logs agent" do
+execute "Install CloudWatch Logs agent" do
   command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r us-east-1 -c /tmp/cwlogs.cfg"
   not_if { system "pgrep -f aws-logs-agent-setup" }
 end
