5a62291940677205bc2df54184fe0fb926a8878a
Colin Mathews <colin.mathews@gmail.com>
2015-03-04 23:07:46 -0500

    Fixed error logging and added system messages

--- logs/attributes/default.rb
+++ logs/attributes/default.rb
@@ -1,3 +1,4 @@
 default[:cwlogs][:logfile] = '/var/log/app'
 default[:cwlogs][:logfileErrors] = '/var/log/app-errors'
 default[:cwlogs][:logfileWarnings] = '/var/log/app-warnings'
+default[:cwlogs][:logfileSystem] = '/var/log/messages'
--- logs/metadata.rb
+++ logs/metadata.rb
@@ -1,2 +1,2 @@
 name             'logs'
-version          '0.0.4'
+version          '0.0.5'
